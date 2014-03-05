b62870f8d3392fad63c7529928c2bbfc526a2646
Daniel Greenberg <dgreenbe777@gmail.com>
2014-03-05 01:06:59 -0500

    Rubymotion Mobile iOS App Cards4Cards

--- app/models/card_collection.rb
+++ app/models/card_collection.rb
@@ -5,4 +5,13 @@ class CardCollection
     self.cards = []
   end
 
+   def initWithCoder(decoder)
+    self.cards = decoder.decodeObjectForKey('cards')
+    self
+  end
+
+  def encodeWithCoder(encoder)
+    encoder.encodeObject(self.cards, forKey:'cards')
+  end
+
 end
